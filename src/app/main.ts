0e9f0d9eafae8accf04059a3f15db50223b21b30
COANelmejor <coanelmejor@gmail.com>
2023-08-16 23:10:22 -0600

    How to extend interfaces

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,8 +2,15 @@ import { addProduct } from "./products/product.service";
 
 addProduct({
   id: 1,
-  title: 'Camiseta',
+  title: 'Shirt',
   createdAt: new Date(),
+  updatedAt: new Date(),
   stock: 100,
-  size: 'S'
+  size: 'S',
+  category: {
+    id: 1,
+    name: 'Clothes',
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }
 })
