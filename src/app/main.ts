52ae823020d2b7b5e73ec6c0e1bd31350e26b543
COANelmejor <coanelmejor@gmail.com>
2023-08-20 00:25:44 -0600

    CRUD example but not completed - Added Faker to the project - Updated Product model - Update use of addProduct in main.ts using faker - Created placeholders for CRUD in product service

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,16 +1,43 @@
-import { addProduct } from "./products/product.service";
+import { faker } from '@faker-js/faker'; // importamos el faker en español
 
-addProduct({
-  id: 1,
-  title: 'Shirt',
-  createdAt: new Date(),
-  updatedAt: new Date(),
-  stock: 100,
-  size: 'S',
-  category: {
-    id: 1,
-    name: 'Clothes',
-    createdAt: new Date(),
-    updatedAt: new Date()
-  }
-})
+import { addProduct, products } from './products/product.service';
+
+for (let i = 0; i < 50; i++) {
+  faker.seed(i); // semilla para obtener datos consistentes
+
+  addProduct({
+    id: faker.string.uuid(),
+    title: faker.commerce.productName(),
+    description: faker.commerce.productDescription(),
+    image: faker.image.url(),
+    color: faker.color.human(),
+    isNew: faker.datatype.boolean(),
+    tags: faker.helpers.multiple(faker.commerce.productAdjective, {
+      count: { min: 1, max: 5 },
+    }),
+    // Posible solución usando arrayElements
+    // tags: faker.helpers.arrayElements(
+    //   [
+    //     faker.commerce.productAdjective(),
+    //     faker.commerce.productAdjective(),
+    //     faker.commerce.productAdjective(),
+    //     faker.commerce.productAdjective(),
+    //     faker.commerce.productAdjective(),
+    //   ],
+    //   { min: 1, max: 5 }
+    // ),
+    price: parseInt(faker.commerce.price({ min: 0, max: 150 }), 10),
+    createdAt: faker.date.past(),
+    updatedAt: faker.date.recent(),
+    stock: faker.number.int({ min: 0, max: 100 }),
+    size: faker.helpers.arrayElement(['S', 'M', 'L', 'XL']),
+    category: {
+      id: 1,
+      name: faker.commerce.department(),
+      createdAt: faker.date.past(),
+      updatedAt: faker.date.recent(),
+    },
+  });
+}
+
+console.log(products);
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -5,3 +5,15 @@ export const products: Product[] = []
 export const addProduct = (data: Product) => {
   products.push(data)
 }
+
+export const updatedProduct = (id: string, data: Product) => {
+  // code
+}
+
+export const getProduct = (id: string) => {
+  // code
+}
+
+export const deleteProduct = (id: string) => {
+  // code
+}
